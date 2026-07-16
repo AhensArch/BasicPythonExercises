d58ee0c3b15fedc35138480ee250bad70f619a42
AhensArch <sneha.gursale705@gmail.com>
2026-07-16 12:19:30 +0400

    added ch02

---
+++ ch02/favourite_number.py
@@ -0,0 +1,7 @@
+# 2-9. Favorite Number: Store your favorite number in a variable. Then, using
+# that variable, create a message that reveals your favorite number. Print that
+# message.
+
+favourite_number = 7
+
+print("Your favourite number is", 64-57)
